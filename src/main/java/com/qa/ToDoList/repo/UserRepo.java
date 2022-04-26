44c7dc057dc38dde34f09d4617c3caa208620742
Nouha Rakeeb <Nouha_Rakeeb@hotmail.co.uk>
2022-04-26 14:57:33 +0100

    NR:added imports in UserRepo and changed class to interface and extended to JpaRespository

--- src/main/java/com/qa/ToDoList/repo/UserRepo.java
+++ src/main/java/com/qa/ToDoList/repo/UserRepo.java
@@ -1,5 +1,11 @@
 package com.qa.ToDoList.repo;
 
-public class UserRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.qa.ToDoList.domain.User;
+
+@Repository //collection of method signatures 
+public interface UserRepo extends JpaRepository <User, Long >{ //methods needs to be used for the database
 
 }
--- src/main/java/com/qa/ToDoList/service/UserService.java
+++ src/main/java/com/qa/ToDoList/service/UserService.java
@@ -1,5 +1,16 @@
 package com.qa.ToDoList.service;
 
+import java.util.List;
+
+
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.qa.ToDoList.domain.User;
+
+
+
 public class UserService {
 
 }
