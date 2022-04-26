24b06bbe2f3ed5d4303853e0186f2eac6a59d70f
Nouha Rakeeb <Nouha_Rakeeb@hotmail.co.uk>
2022-04-26 10:42:32 +0100

    NR: created UserController file

---
+++ src/main/java/com/qa/ToDoList/Controller/UserController.java
@@ -0,0 +1,22 @@
+package com.qa.ToDoList.Controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/Notes")
+public class UserController {
+
+}
