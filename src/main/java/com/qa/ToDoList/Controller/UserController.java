d1da00b97a4e35df31ee0218c141ed8cd30cb92e
Nouha Rakeeb <Nouha_Rakeeb@hotmail.co.uk>
2022-04-26 11:40:29 +0100

    NR: outlined get, put, delete, etc and did getmapping for get all posts

--- src/main/java/com/qa/ToDoList/Controller/UserController.java
+++ src/main/java/com/qa/ToDoList/Controller/UserController.java
@@ -14,9 +14,31 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.qa.ToDoList.domain.User;
+
+
+
 @RestController
 @CrossOrigin
 @RequestMapping("/Notes")
 public class UserController {
 
+	//GET
+	@GetMapping("/getAll") //localhost:8080/getAll
+	public ResponseEntity<List<User>> getAll() {
+		return new ResponseEntity<List<User>>(service.getAll(),HttpStatus.OK);
+	}
+	
+	
+	//POST
+	
+	
+	
+	//PUT
+	
+	
+	
+	
+	//DELETE
+	
 }
