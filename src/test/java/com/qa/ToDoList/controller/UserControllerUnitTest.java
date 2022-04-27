49e0488b5024000429b3d621ed5652e7b0642db8
Nouha Rakeeb <Nouha_Rakeeb@hotmail.co.uk>
2022-04-27 18:37:02 +0100

    NR: created test class for create notes and Mockito to mock the response

--- src/test/java/com/qa/ToDoList/controller/UserControllerUnitTest.java
+++ src/test/java/com/qa/ToDoList/controller/UserControllerUnitTest.java
@@ -1,5 +1,50 @@
 package com.qa.ToDoList.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.qa.ToDoList.domain.User;
+import com.qa.ToDoList.service.UserService;
+
+
+@WebMvcTest
 public class UserControllerUnitTest {
 
+		@Autowired
+		private MockMvc mvc;
+		
+		@Autowired
+		private ObjectMapper mapper;
+		
+		@MockBean
+		private UserService service;
+		
+		@Test
+		void createTest() throws Exception {
+			User entry = new User("Food List", "Pasta\\nBread\\nApples\\nChicken\\nMilk ", "£40");
+			String entryAsJSON = this.mapper.writeValueAsString(entry);
+			
+			Mockito.when(this.service.create(entry)).thenReturn(entry);
+			
+			mvc.perform(post("/Notes/create")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(entryAsJSON))
+				.andExpect(status().isCreated())
+				.andExpect(content().json(entryAsJSON));
+		}
+	
 }
