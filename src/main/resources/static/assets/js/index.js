cacbf98e5d5e9f35231613dd1d1b2b5ada7bc2ef
Nouha Rakeeb <Nouha_Rakeeb@hotmail.co.uk>
2022-04-29 10:21:28 +0100

    NR: created a function to get all todo from the database. Once data is recieved it will call onto another funtion called makeTodo

--- src/main/resources/static/assets/js/index.js
+++ src/main/resources/static/assets/js/index.js
@@ -3,3 +3,17 @@ const ADDR = "http://localhost:8080";
 
 //calling function
 getAllTodoList()
+
+// GET ALL TODO
+function getAllTodoList() {
+    return axios.get(`${ADDR}/Notes/getAll`)
+        .then(
+            function (res) {
+
+                document.getElementById("taskList").innerHTML = "";
+
+                res.data.forEach(makeTodo);
+            }
+        )
+        .catch((err) => console.error(err))
+}
