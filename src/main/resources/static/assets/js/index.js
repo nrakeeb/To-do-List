fd663e40be70cdbeed15386dca0ffe4c6f95bbd9
Nouha Rakeeb <Nouha_Rakeeb@hotmail.co.uk>
2022-04-29 10:23:34 +0100

    NR: for each response getAll function makes, the Maketodo function creates a list of the response and is appended to elements and attributes in the makeTodo function

--- src/main/resources/static/assets/js/index.js
+++ src/main/resources/static/assets/js/index.js
@@ -17,3 +17,52 @@ function getAllTodoList() {
         )
         .catch((err) => console.error(err))
 }
+
+
+// TODO DOM ELEMENTS AND ATTRIBUTES
+function makeTodo(todoItem) {
+    const task = document.createElement("div");
+    task.className = "task";
+    task.id = todoItem.id;
+
+    const taskContent = document.createElement("div");
+    taskContent.classNames = "content";
+
+    const inputTag = document.createElement("input");
+
+    inputTag.id = "input_" + todoItem.id;
+    inputTag.type = "text";
+    inputTag.className = "text";
+    inputTag.value = todoItem.comments;
+    inputTag.readOnly = true;
+
+
+    taskContent.appendChild(inputTag);
+    task.appendChild(taskContent);
+
+    const action = document.createElement("div");
+    action.className = "actions";
+
+    const updateButton = document.createElement("button");
+    updateButton.innerText = "update";
+    updateButton.id = "update_" + todoItem.id;
+    updateButton.className = "update";
+    updateButton.onclick = function () {
+        toggleUpdate(todoItem.id);
+    }
+
+    const completeButton = document.createElement("button");
+    completeButton.innerText = "complete";
+    completeButton.id = "complete_" + todoItem.id;
+    completeButton.className = "complete";
+    completeButton.onclick = function () {
+        deleteToDoList(todoItem.id);
+    }
+
+    action.appendChild(updateButton);
+    action.appendChild(completeButton);
+
+    task.appendChild(action);
+
+    document.getElementById("taskList").appendChild(task)
+}
