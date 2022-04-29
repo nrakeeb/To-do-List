11c066083b0302b6f1bb85ed34cebfe63c7a71ff
Nouha Rakeeb <Nouha_Rakeeb@hotmail.co.uk>
2022-04-29 03:11:38 +0100

    NR: created Userservice test class

---
+++ src/test/java/com/qa/ToDoList/service/UserServiceTest.java
@@ -0,0 +1,17 @@
+package com.qa.ToDoList.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import com.qa.ToDoList.domain.User;
+import com.qa.ToDoList.repo.UserRepo;
+
+public class UserServiceTest {
+
+}
